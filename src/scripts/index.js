60ff706ae81018c5de5f23e59cf77614ea77d01b
Sajeeb <sajeebahamed6@gmail.com>
2020-07-31 02:34:35 +0600

    loader/plugins

---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+import { ABC } from './test'
+import '../styles/index.scss'
+const fn = () => {
+    console.log("Helloo");
+}
+
+fn()
+
+console.log(ABC)
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const htmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/scripts/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: 'html-loader'
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+        new htmlWebPackPlugin({
+            template: './src/index.html'
+        })
+    ]
+    
+}
