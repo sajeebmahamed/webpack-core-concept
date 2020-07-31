d220ad0a938ad2129a86eae54eaa07713b0dcc77
Sajeeb <sajeebahamed6@gmail.com>
2020-07-31 16:49:33 +0600

    basic dom

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,25 @@
-import { ABC } from './test'
 import '../styles/index.scss'
-const fn = () => {
-    console.log("Helloo");
-}
 
-fn()
+// const h1 = document.getElementById('my-id')
+// h1.innerHTML = 'Sajeeb Ahamed'
+// console.log(h1);]
 
-console.log(ABC)
+let header = document.getElementsByTagName('h1')
+let para = document.getElementsByClassName('lead')
+// console.log(para);
+
+let btn = document.getElementById('myBtn')
+let colors = ['red', 'green', 'blue', 'yello']
+let i = 0
+btn.addEventListener('click', function() {
+    let title =  document.getElementById('my-id')
+    title.innerHTML = 'Hey! Man. Whats up??'
+    title.style.backgroundColor = colors[i]
+
+    if(i>= colors.length) {
+        i = 0
+    }
+    else {
+        i++
+    }
+})
